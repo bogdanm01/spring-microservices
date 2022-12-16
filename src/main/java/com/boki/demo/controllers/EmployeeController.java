a24a74f8f21fa62cdc7c4c0646731bb604e1d45d
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2022-12-16 17:06:14 +0100

    Update EmployeeController.java

--- src/main/java/com/boki/demo/controllers/EmployeeController.java
+++ src/main/java/com/boki/demo/controllers/EmployeeController.java
@@ -2,6 +2,7 @@ package com.boki.demo.controllers;
 
 import com.boki.demo.models.Employee;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -24,5 +25,11 @@ public class EmployeeController {
     @ResponseStatus(HttpStatus.OK)
     public Employee getEmployeeById(@PathVariable int id){
         return mockData.get(id - 1);
+    }
+
+    @PostMapping("insertEmployee")
+    public ResponseEntity<?> insertEmployee(@RequestBody Employee employee) {
+        // TODO: Implement insert employee endpoint
+        return null;
     }
 }
