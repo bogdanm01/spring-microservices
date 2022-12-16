b7de6ecc8b6c42968f3f936d9729d3a1978c437b
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2022-12-16 17:06:11 +0100

    Update Employee.java

--- src/main/java/com/boki/demo/models/Employee.java
+++ src/main/java/com/boki/demo/models/Employee.java
@@ -1,22 +1,25 @@
 package com.boki.demo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.Id;
 import java.util.Objects;
 
-@Entity
+
+//@Entity
 @Getter
 @Setter
 @EqualsAndHashCode
 @ToString
 public class Employee {
 
+    @JsonIgnore
     private @Id @GeneratedValue Long id;
     private String name;
     private String role;
@@ -35,5 +38,4 @@ public class Employee {
     public Employee() {
 
     }
-
 }
